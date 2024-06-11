9ce369b085dd4a3eca13ce9b8846fcc923ba0f30
Sansuiz <sansui_z@126.com>
2024-06-11 22:02:29 +0800

    Update snow.js

--- js/snow.js
+++ js/snow.js
@@ -8,7 +8,7 @@ if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobil
       let e = {
           flakeCount: 50,
           minDist: 150,
-          color: "255, 255, 255",
+          color: "43, 87, 129",
           size: 2,
           speed: .5,
           opacity: .2,

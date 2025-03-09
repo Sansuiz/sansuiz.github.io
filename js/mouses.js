0743e84b6ccb0117a30fc4e4e9c039f2efa60a86
Sansuiz <sansui_z@126.com>
2025-03-09 19:48:27 +0800

    Update mouses.js

--- js/mouses.js
+++ js/mouses.js
@@ -55,8 +55,14 @@ class Cursor {
     }
 
     init() {
-        document.onmouseover  = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.add("hover");
-        document.onmouseout   = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.remove("hover");
+        document.onmouseover  = e => {
+          this.pt.includes(e.target.outerHTML) && this.cursor.classList.add("hover");
+          if(e.target.tagName.toLowerCase() === 'a') this.cursor.classList.add("hover");
+        };
+        document.onmouseout   = e => {
+          this.pt.includes(e.target.outerHTML) && this.cursor.classList.remove("hover");
+          if(e.target.tagName.toLowerCase() === 'a') this.cursor.classList.remove("hover");
+        };
         document.onmousemove  = e => {(this.pos.curr == null) && this.move(e.clientX - 8, e.clientY - 8); this.pos.curr = {x: e.clientX - 8, y: e.clientY - 8}; this.cursor.classList.remove("hidden");};
         document.onmouseenter = e => this.cursor.classList.remove("hidden");
         document.onmouseleave = e => this.cursor.classList.add("hidden");

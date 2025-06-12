ca2408d707af59dae14a38072d88b38afbc78950
Sansuiz <sansui_z@126.com>
2025-06-12 17:59:13 +0800

    分页调整

--- js/shared.js
+++ js/shared.js
@@ -9,3 +9,13 @@ window.addEventListener('DOMContentLoaded', () => {
   cursor.className = 'custom-cursor';
   document.body.appendChild(cursor);
 });
+
+// 在文件底部添加
+window.addEventListener('load', function() {
+  new fullpage('#fullpage', {
+    scrollingSpeed: 1000,
+    navigation: true,
+    navigationPosition: 'right',
+    credits: { enabled: false }
+  });
+});

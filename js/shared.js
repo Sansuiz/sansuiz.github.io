3829b893b4ad5632c6433262a98453f3e3fc304a
Sansuiz <sansui_z@126.com>
2025-06-12 14:41:55 +0800

    调试功能

--- js/shared.js
+++ js/shared.js
@@ -8,4 +8,35 @@ window.addEventListener('DOMContentLoaded', () => {
   const cursor = document.createElement('div');
   cursor.className = 'custom-cursor';
   document.body.appendChild(cursor);
+
+  // 画廊交互
+  let startY;
+  const gallery = document.querySelector('.gallery-container');
+  const threshold = 100; // 滑动阈值
+  
+  window.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY;
+  });
+  
+  window.addEventListener('touchmove', (e) => {
+    if (!startY) return;
+    
+    const y = e.touches[0].clientY;
+    const diff = startY - y;
+    
+    if (diff > threshold && !gallery.classList.contains('active')) {
+      gallery.classList.add('active');
+    } else if (diff < -threshold && gallery.classList.contains('active')) {
+      gallery.classList.remove('active');
+    }
+  });
+  
+  // 鼠标滚轮交互
+  window.addEventListener('wheel', (e) => {
+    if (e.deltaY > 50 && !gallery.classList.contains('active')) {
+      gallery.classList.add('active');
+    } else if (e.deltaY < -50 && gallery.classList.contains('active')) {
+      gallery.classList.remove('active');
+    }
+  });
 });

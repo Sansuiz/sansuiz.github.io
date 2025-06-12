ae91b38b72574962cf7d6905f572ce4557a78f7d
Sansuiz <sansui_z@126.com>
2025-06-12 14:52:11 +0800

    回退版本

--- js/shared.js
+++ js/shared.js
@@ -9,13 +9,3 @@ window.addEventListener('DOMContentLoaded', () => {
   cursor.className = 'custom-cursor';
   document.body.appendChild(cursor);
 });
-
-window.addEventListener('wheel', (e) => {
-  if (e.deltaY > 0) {
-    document.getElementById('page1').style.display = 'none';
-    document.getElementById('page2').style.display = 'block';
-  } else {
-    document.getElementById('page1').style.display = 'block';
-    document.getElementById('page2').style.display = 'none';
-  }
-});

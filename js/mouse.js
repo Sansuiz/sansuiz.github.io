f01010e2ab6230acb6a0130b0d8de62d624f9ea8
Sansuiz <sansui_z@126.com>
2025-06-17 18:20:24 +0800

    修复鼠标样式效果

--- js/mouse.js
+++ js/mouse.js
@@ -192,3 +192,5 @@ document.querySelectorAll('.photo-frame').forEach(frame => {
     document.querySelector('.hover-background').style.opacity = '0';
   });
 });
+// 在动态加载内容的回调函数中添加
+if(window.CURSOR) CURSOR.refresh();

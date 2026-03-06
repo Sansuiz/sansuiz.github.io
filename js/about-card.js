dc5abcf6c01e2c9b17e5a1d33fd362566dc69de5
Sansuiz <sansui_z@126.com>
2026-03-06 23:22:14 +0800

    优化样式

--- js/about-card.js
+++ js/about-card.js
@@ -2,13 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const card = document.querySelector('.card');
   
   if (card) {
-    card.addEventListener('click', function(e) {
-      if (e.target.closest('.site-link-icon')) {
-        return;
-      }
-      if (!e.target.closest('.timeline-container')) {
-        card.classList.toggle('flipped');
-      }
+    card.addEventListener('click', function() {
+      card.classList.toggle('flipped');
     });
   }
 });
